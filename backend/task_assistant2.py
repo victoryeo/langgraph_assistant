b9068ddd4cc9867c20c80e7c06f5ebbcb2097c45
victoryeo <victoryeo1974@gmail.com>
2025-07-21 12:46:09 +0800

    use new langchain_huggingface

--- backend/task_assistant2.py
+++ backend/task_assistant2.py
@@ -5,7 +5,8 @@ from langchain_groq import ChatGroq
 from langchain_core.documents import Document
 from langchain_community.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
 from datetime import datetime, timedelta
