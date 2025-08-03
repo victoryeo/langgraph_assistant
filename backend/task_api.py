884f38535c88cf662347012a47b928a50d5e90ba
victoryeo <victoryeo1974@gmail.com>
2025-08-03 22:37:20 +0800

    fix mismatch between how you create the JWT token and how you validate it.

--- backend/task_api.py
+++ backend/task_api.py
@@ -193,7 +193,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
+        email: str = payload.get("email")
         if email is None:
             raise credentials_exception
         token_data = TokenData(email=email)
@@ -320,7 +320,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
+        data={"email": user.email}, expires_delta=access_token_expires
     )
     
     return {"access_token": access_token, "token_type": "bearer"}
